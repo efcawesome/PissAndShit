896f4f1b929dbfcf5e829e6e34faafd1ffc99b02
musicman655 <66842165+musicman655@users.noreply.github.com>
2020-07-21 10:47:46 -0700

    Add files via upload

---
+++ AGlobalTile.cs
@@ -0,0 +1,31 @@
+using System.Collections.Generic;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace testmod
+
+{
+    public class AGlobalTile : GlobalTile
+    {
+        public override bool Drop(int a, int b, int type)
+        {	
+			Player player = Main.LocalPlayer;
+            if (Main.netMode != 1 && !WorldGen.noTileActions && !WorldGen.gen)
+            {
+                if (type == TileID.Trees && Main.tile[a, b + 1].type == TileID.Grass)
+                {
+					if (Main.rand.Next(3) == 0)
+					{
+						NPC.NewNPC((int)player.position.X, (int)player.position.Y - 75, mod.NPCType("CoochieSnatcher"));
+					}
+                }
+			}
+			return base.Drop(a, b, type);
+        }
+
+    }
+
+}
