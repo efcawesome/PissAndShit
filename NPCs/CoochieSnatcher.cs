47ab06ad3eb45e3cf8c0274931ebd11915309f94
musicman655 <66842165+musicman655@users.noreply.github.com>
2020-07-21 10:44:04 -0700

    Add files via upload

---
+++ NPCs/CoochieSnatcher.cs
@@ -0,0 +1,39 @@
+using System;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace testmod.NPCs
+{
+	public class CoochieSnatcher : ModNPC
+	{	
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Coochie Snatcher");
+		}
+
+		public override void SetDefaults()
+		{
+			npc.width = 64;
+			npc.height = 64;
+			npc.damage = 28;
+			npc.defense = 12;
+			npc.lifeMax = 1;
+			npc.HitSound = SoundID.NPCHit5;
+			npc.DeathSound = SoundID.NPCDeath5;
+			npc.value = 31f;
+			npc.knockBackResist = 0.2f;
+            npc.aiStyle = 30;
+			npc.noGravity = true;
+			npc.noTileCollide = true;
+		}
+		
+		public override void NPCLoot()
+		{
+
+		}
+	}
+}
