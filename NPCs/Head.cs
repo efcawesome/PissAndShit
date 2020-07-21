1d4b6f1f7bc85ef7ea63ca3c80307f4c2b6d91b5
tigershark5639 <68576691+tigershark5639@users.noreply.github.com>
2020-07-21 17:47:33 -0400

    Siren head in solar eclishpe lol

---
+++ NPCs/Head.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace pissandshit.NPC
+{
+
+	public class Head : ModNPC
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Siren Head");
+			Main.npcFrameCount[npc.type] = 1;
+		}
+
+		public override void SetDefaults()
+		{
+			npc.width = 188;
+			npc.height = 175;
+			npc.aiStyle = 38;
+			npc.damage = 30;
+			npc.defense = 5;
+			npc.lifeMax = 1500;
+			npc.HitSound = SoundID.NPCHit55;
+			npc.DeathSound = SoundID.NPCDeath59;
+			npc.value = 25f;
+			npc.knockBackResist = 0.5f;
+			npc.scale = 1f;
+		}
+		public override float SpawnChance(NPCSpawnInfo spawnInfo)
+		{
+			return SpawnCondition.SolarEclipse.Chance * 0.05f;
+
+		}
+	}
+}
