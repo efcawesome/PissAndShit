ad3c3fd9392e601718c2119c8508aef8b58f3c1e
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-07-22 14:21:40 -0700

    added god slime's gel and bad time medallion (needs sprite)

---
+++ Items/Accessories/BadTimeMedallion.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CringeMod.Items.Accessories
+{
+    class BadTimeMedallion : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.width = 32;
+            item.height = 30;
+
+            item.value = Item.buyPrice(gold: 50);
+            item.rare = ItemRarityID.Expert;
+            item.accessory = true;
+            item.expert = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            if(player.statLife > player.statLifeMax/4)
+            {
+                player.allDamageMult = 1.15f;
+            }
+            else if(player.statLife <= player.statLifeMax/4)
+            {
+                player.allDamageMult = 1.3f;
+            }
+        }
+    }
+}
---
+++ Items/Accessories/GodSlimesGel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CringeMod.Items.Accessories
+{
+    class GodSlimesGel : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("God Slime's Gel");
+            Tooltip.SetDefault("Makes you jump H I G H");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 32;
+            item.height = 30;
+
+            item.value = Item.buyPrice(gold: 50);
+            item.rare = ItemRarityID.Expert;
+            item.accessory = true;
+            item.expert = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            Player.jumpHeight += 400;
+            Player.jumpSpeed += 50;
+            player.noFallDmg = true;
+        }
+    }
+}
