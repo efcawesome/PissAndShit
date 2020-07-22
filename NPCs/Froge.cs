fb5487e2045228a6a8f9bcf8a4e0d42437488573
CalEatsLasagna <40683183+CalEatsLasagna@users.noreply.github.com>
2020-07-21 22:47:10 -0600

    Added Aquarian and Froge
    Froge has no spawn condition yet

---
+++ NPCs/Froge.cs
@@ -0,0 +1,64 @@
+using System;
+using System.IO;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.NPCs
+{
+	public class Froge : ModNPC
+	{
+		private int frameNum = 0;
+		private int frameTimer = 0;
+		int timer = 0;
+		
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Froge");
+			NPCID.Sets.MustAlwaysDraw[npc.type] = true;
+			Main.npcFrameCount[npc.type] = 53;
+		}
+
+		public override void SetDefaults()
+		{
+			npc.width = 112;
+			npc.height = 112;
+			npc.damage = 1;
+			npc.defense = 10;
+			npc.lifeMax = 2000;
+			npc.HitSound = SoundID.NPCHit1;
+			npc.DeathSound = SoundID.NPCHit2;
+			npc.value = 60f;
+			npc.knockBackResist = 0.5f;
+            npc.aiStyle = 5;
+			npc.lavaImmune = true;
+			npc.noGravity = true;
+			npc.noTileCollide = true;	
+		}
+		
+        public override void AI()
+        {
+			frameTimer++;
+        }
+		
+		public override void FindFrame(int frameHeight)
+		{
+			npc.spriteDirection = npc.direction;
+
+			if (frameTimer == 2)
+			{
+				frameNum++;
+				frameTimer = 0;
+			}
+			
+			if (frameNum == 53)
+			{
+				frameNum = 0;
+			}
+			
+			npc.frame.Y = frameNum * frameHeight;
+		}
+	}
+}
