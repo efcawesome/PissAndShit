da8a25259423753e9a8b34f26256ee2db17e157f
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-07-22 14:02:41 -0700

    Add files via upload
    lmao

---
+++ Items/JarOfMoonMilk.cs
@@ -0,0 +1,28 @@
+﻿using IL.Terraria.GameContent.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CringeMod.Items
+{
+    class JarOfMoonMilk : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Jar of Moon Milk");
+            Tooltip.SetDefault("Milk of the Moon Lord *UwU*");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 64;
+            item.height = 64;
+            item.maxStack = 1;
+            item.rare = ItemRarityID.Red;
+        }
+    }
+}
