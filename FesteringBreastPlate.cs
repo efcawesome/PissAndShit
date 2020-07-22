d345da910c1e7c15cae9484f8a82517faf968608
ArnoldPalmergolf <39205784+aadiuchil@users.noreply.github.com>
2020-07-22 12:09:19 -0400

    Added Poop armors (called festering armors)
    Poop Armors can't be crafted yet and they give you insane DR and defence, set bonus is 500 extra health

---
+++ FesteringBreastPlate.cs
@@ -0,0 +1,36 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace PissAndShit.Items.Armor
+{
+	[AutoloadEquip(EquipType.Body)]
+	public class FesteringBreastPlate : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			base.SetStaticDefaults();
+			DisplayName.SetDefault("Festering Breastplate");
+			Tooltip.SetDefault("it smells like poop."
+				+ "\nGives a ton of DR and defense'");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 18;
+			item.height = 18;
+			item.value = 10000;
+			item.rare = ItemRarityID.Purple;
+			item.defense = 60;
+		}
+
+		public override void UpdateEquip(Player player)
+		{
+			player.statDefense += 150;
+			player.endurance += 130;
+		}
+
+		
+	}
+}
