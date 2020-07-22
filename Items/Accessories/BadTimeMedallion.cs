9f979d3e85bca307cb59e06c1c060f5c892a2f4c
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-07-22 14:22:16 -0700

    fixed namespace

--- Items/Accessories/BadTimeMedallion.cs
+++ Items/Accessories/BadTimeMedallion.cs
@@ -7,7 +7,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace CringeMod.Items.Accessories
+namespace PissAndShit.Items.Accessories
 {
     class BadTimeMedallion : ModItem
     {
