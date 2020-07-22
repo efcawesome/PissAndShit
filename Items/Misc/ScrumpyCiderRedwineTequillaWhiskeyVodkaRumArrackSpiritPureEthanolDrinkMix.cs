b22d8bdaeaa7e0e10f32525c72b6d6c3b728c2e1
CalEatsLasagna <40683183+CalEatsLasagna@users.noreply.github.com>
2020-07-22 15:46:53 -0600

    Directory update

--- Items/Misc/ScrumpyCiderRedwineTequillaWhiskeyVodkaRumArrackSpiritPureEthanolDrinkMix.cs
+++ Items/Misc/ScrumpyCiderRedwineTequillaWhiskeyVodkaRumArrackSpiritPureEthanolDrinkMix.cs
@@ -2,7 +2,7 @@
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace PissAndShit.Items
+namespace PissAndShit.Items.Misc
 {
     public class ScrumpyCiderRedwineTequillaWhiskeyVodkaRumArrackSpiritPureEthanolDrinkMix : ModItem
     {
