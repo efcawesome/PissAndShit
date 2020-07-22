8142b16e81d9ab9820a2e0c1e4fcaa7931b8ec91
musicman655 <66842165+musicman655@users.noreply.github.com>
2020-07-22 14:10:42 -0700

    boss dump

---
+++ NPCs/Bosses/YoungDuke.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.NPCs
+{
+	class YoungDuke : ModNPC
+	{
+		public override string Texture => "Terraria/NPC_" + NPCID.DukeFishron;
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Young Duke");
+			Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.DukeFishron];
+		}
+
+		public override void SetDefaults()
+		{
+			npc.CloneDefaults(NPCID.DukeFishron);
+			aiType = NPCID.DukeFishron;
+			animationType = NPCID.DukeFishron;
+		}
+	}
+}
