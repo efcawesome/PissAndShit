28d7775c23d62de5683fe08592287f0ab0bf2abc
musicman655 <66842165+musicman655@users.noreply.github.com>
2020-07-22 14:31:32 -0700

    Add files via upload

---
+++ Items/Misc/HallowedBottle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Items
+{
+    public class HallowedBottle: ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Hallowed Bottle");
+            Tooltip.SetDefault("King Arthur's bottle of choice");
+        }
+        public override void SetDefaults()
+        {
+            item.maxStack = 999;
+            item.rare = 6;
+            item.width = 20;
+            item.height = 26;
+
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.HallowedBar, 3);
+            recipe.AddTile(TileID.MythrilAnvil);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
+
+    
+
