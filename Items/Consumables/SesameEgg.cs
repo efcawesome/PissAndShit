7b53d6eaf5820c81fa7bcb493440bd3c12b37b03
jamespuzzle1 <68666897+jamespuzzle1@users.noreply.github.com>
2020-07-22 22:00:27 -0700

    Create SesameEgg.cs

---
+++ Items/Consumables/SesameEgg.cs
@@ -0,0 +1,44 @@
+using PissAndShit.NPCs.Bosses
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CringeMod.Items.Consumables
+{
+    class SesameEgg : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.width = 14;
+            item.height = 20;
+            item.maxStack = 20;
+            item.useStyle = ItemUseStyleID.HoldingUp;
+            item.useTime = 45;
+            item.useAnimation = 45;
+            item.consumable = true;
+            item.rare = ItemRarityID.Red;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            return !NPC.AnyNPCs(mod.NPCType("EggBert"));
+        }
+
+        public override bool UseItem(Player player)
+        {
+            Main.PlaySound(SoundID.Roar, player.position);
+            if (Main.netMode != NetmodeID.MultiplayerClient)
+            {
+                NPC.SpawnOnPlayer(player.whoAmI, mod.NPCType("EggBert"));
+            }
+            return true;
+        }
+
+       
+    }
+}
