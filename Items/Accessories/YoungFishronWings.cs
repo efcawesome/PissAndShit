f7a87c58b00c30367f868e622c498746fe3dfb39
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-07-23 01:07:56 -0700

    young fishron wings with PLACEHOLDER sprites

---
+++ Items/Accessories/YoungFishronWings.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CringeMod.Items.Accessories
+{
+    [AutoloadEquip(EquipType.Wings)]
+    class YoungFishronWings : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+			DisplayName.SetDefault("Young Fishron Wings");
+        }
+        public override void SetDefaults()
+		{
+			item.width = 22;
+			item.height = 20;
+			item.value = Item.sellPrice(gold: 1);
+			item.rare = ItemRarityID.Green;
+			item.accessory = true;
+		}
+		public override void UpdateAccessory(Player player, bool hideVisual)
+		{
+			player.wingTimeMax = 60;
+		}
+
+		public override void VerticalWingSpeeds(Player player, ref float ascentWhenFalling, ref float ascentWhenRising,
+			ref float maxCanAscendMultiplier, ref float maxAscentMultiplier, ref float constantAscend)
+		{
+			ascentWhenFalling = 0.55f;
+			ascentWhenRising = 0.05f;
+			maxCanAscendMultiplier = 1f;
+			maxAscentMultiplier = 1f;
+			constantAscend = 0.135f;
+		}
+
+		public override void HorizontalWingSpeeds(Player player, ref float speed, ref float acceleration)
+		{
+			speed = 3f;
+			acceleration *= 1.1f;
+		}
+	}
+}
