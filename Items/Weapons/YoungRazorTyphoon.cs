a751eec4cd3e6ec1751c9c8ae8c70b5aaad51eea
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-07-23 14:36:56 -0700

    Young razorblade typhoon

---
+++ Items/Weapons/YoungRazorTyphoon.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CringeMod.Items.Weapons
+{
+    class YoungRazorTyphoon : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Young Razorblade Typhoon");
+        }
+
+        public override void SetDefaults()
+        {
+            item.damage = 20;
+            item.magic = true;
+            item.width = 22;
+            item.height = 24;
+            item.useTime = 25;
+            item.useAnimation = 25;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.noMelee = true;
+            item.knockBack = 0.5f;
+            item.value = Item.sellPrice(gold: 1, silver: 50);
+            item.rare = ItemRarityID.Green;
+            item.mana = 8;
+            item.UseSound = SoundID.Item84;
+            item.autoReuse = true;
+            item.shoot = ProjectileID.Typhoon;
+            item.shootSpeed = 6;
+        }
+    }
+}
