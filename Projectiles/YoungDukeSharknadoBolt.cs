5791811aff31e73f1b31600824743360e366b714
ArnoldPalmergolf <39205784+aadiuchil@users.noreply.github.com>
2020-07-23 14:20:38 -0400

    YoungDukeSharknadoBolt now summons shark nado

--- Projectiles/YoungDukeSharknadoBolt.cs
+++ Projectiles/YoungDukeSharknadoBolt.cs
@@ -8,14 +8,14 @@ using System.Threading.Tasks;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
-
+using static Terraria.ModLoader.ModContent;
 namespace PissAndShit.Projectiles
 {
 	class YoungDukeSharknadoBolt : ModProjectile
 	{
 		public override void SetStaticDefaults()
 		{
-			DisplayName.SetDefault("Young Duke's Sharknado Bolt");
+			DisplayName.SetDefault("Young Sharknado Bolt");
 			Main.projFrames[projectile.type] = 3;
 		}
 		public override void SetDefaults()
@@ -27,7 +27,7 @@ namespace PissAndShit.Projectiles
 			projectile.aiStyle = 65;
 			projectile.alpha = 255;
 			projectile.timeLeft = 300;
-			aiType = ProjectileID.SharknadoBolt;
+			ProjectileID.Sets.MinionShot[projectile.type] = true;
 			ProjectileID.Sets.TrailCacheLength[projectile.type] = 5;
 			ProjectileID.Sets.TrailingMode[projectile.type] = 0;
 
@@ -42,10 +42,65 @@ namespace PissAndShit.Projectiles
 		public float[] localAI = new float[maxAI];
 		public override void Kill(int timeLeft)
 		{
-
+			Main.PlaySound(SoundID.NPCKilled, (int)projectile.Center.X, (int)projectile.Center.Y, 19);
+			int num252 = 36;
+			for (int num253 = 0; num253 < num252; num253++)
+			{
+				Vector2 spinningpoint2 = Vector2.Normalize(projectile.velocity) * new Vector2((float)projectile.width / 2f, projectile.height) * 0.75f;
+				spinningpoint2 = spinningpoint2.RotatedBy((float)(num253 - (num252 / 2 - 1)) * ((float)Math.PI * 2f) / (float)num252) + projectile.Center;
+				Vector2 vector6 = spinningpoint2 - projectile.Center;
+				int num254 = Dust.NewDust(spinningpoint2 + vector6, 0, 0, 172, vector6.X * 2f, vector6.Y * 2f, 100, default(Color), 1.4f);
+				Main.dust[num254].noGravity = true;
+				Main.dust[num254].noLight = true;
+				Main.dust[num254].velocity = vector6;
+			}
+			if (projectile.owner == Main.myPlayer)
+			{
+				if (projectile.ai[1] < 1f)
+				{
+					int num255 = Main.expertMode ? 25 : 40;
+					int num256 = Projectile.NewProjectile(projectile.Center.X - (float)(projectile.direction * 30), projectile.Center.Y - 4f, (float)(-projectile.direction) * 0.01f, 0f, ProjectileType<Projectiles.YoungDukeSharknado>(), num255, 4f, projectile.owner, 16f, 15f);
+					Main.projectile[num256].netUpdate = true;
+				}
+				else
+				{
+					int num258 = (int)(projectile.Center.Y / 16f);
+					int num259 = (int)(projectile.Center.X / 16f);
+					int num260 = 100;
+					if (num259 < 10)
+					{
+						num259 = 10;
+					}
+					if (num259 > Main.maxTilesX - 10)
+					{
+						num259 = Main.maxTilesX - 10;
+					}
+					if (num258 < 10)
+					{
+						num258 = 10;
+					}
+					if (num258 > Main.maxTilesY - num260 - 10)
+					{
+						num258 = Main.maxTilesY - num260 - 10;
+					}
+					for (int num261 = num258; num261 < num258 + num260; num261++)
+					{
+						Tile tile = Main.tile[num259, num261];
+						if (tile.active() && (Main.tileSolid[tile.type] || tile.liquid != 0))
+						{
+							num258 = num261;
+							break;
+						}
+					}
+					int num262 = Main.expertMode ? 50 : 80;
+					int num263 = Projectile.NewProjectile(num259 * 16 + 8, num258 * 16 - 24, 0f, 0f, ProjectileID.Cthulunado, num262, 4f, Main.myPlayer, 16f, 24f);
+					Main.projectile[num263].netUpdate = true;
+				}
+			}
 		}
 		public override void AI()
 		{
+
 			if (projectile.ai[1] > 0f)
 			{
 				int num784 = (int)projectile.ai[1] - 1;
@@ -88,7 +143,42 @@ namespace PissAndShit.Projectiles
 					}
 				}
 			}
+			else
+			{
+				float num790 = (float)Math.PI / 15f;
+				float num791 = 4f;
+				float num792 = (float)(Math.Cos(num790 * projectile.ai[0]) - 0.5) * num791;
+				projectile.velocity.Y -= num792;
+				projectile.ai[0]++;
+				num792 = (float)(Math.Cos(num790 * projectile.ai[0]) - 0.5) * num791;
+				projectile.velocity.Y += num792;
+				localAI[0]++;
+				if (localAI[0] > 10f)
+				{
+					projectile.alpha -= 5;
+					if (projectile.alpha < 100)
+					{
+						projectile.alpha = 100;
+					}
+					projectile.rotation += projectile.velocity.X * 0.1f;
+					projectile.frame = (int)(localAI[0] / 3f) % 3;
+				}
+			}
+			if (projectile.wet)
+			{
+				projectile.position.Y -= 16f;
+				projectile.Kill();
+			}
+
+			if (Main.rand.Next(2) == 0)
+			{
+				int num234 = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 14);
+				Main.dust[num234].velocity.X *= 0.4f;
+			}
+
 		}
 	}
-}
+	}
+	
+
 
