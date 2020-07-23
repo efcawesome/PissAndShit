0aeec463bf9344cfbe429fda0f80c45549d5d604
ArnoldPalmergolf <39205784+aadiuchil@users.noreply.github.com>
2020-07-23 14:26:43 -0400

    Fixed Namespace

--- Items/Weapons/SansYoYo.cs
+++ Items/Weapons/SansYoYo.cs
@@ -5,7 +5,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 
-namespace PissAndShit.Items
+namespace PissAndShit.Items.Weapons
 {
 	public class SansYoyo : ModItem
 	{
