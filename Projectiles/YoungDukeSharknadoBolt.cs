3a744841b1be06045a0ca11eb9a87f09c68b6044
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-07-23 00:03:05 -0700

    needs dust and to spawn sharknado on contact with anything

---
+++ Projectiles/YoungDukeSharknadoBolt.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CringeMod.Projectiles
+{
+    class YoungDukeSharknadoBolt : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Young Duke's Sharknado Bolt");
+            Main.projFrames[projectile.type] = 3;
+        }
+        public override void SetDefaults()
+        {
+            projectile.CloneDefaults(ProjectileID.SharknadoBolt);
+            projectile.width = 14;
+            projectile.height = 18;
+            aiType = ProjectileID.SharknadoBolt;
+            ProjectileID.Sets.TrailCacheLength[projectile.type] = 5;
+            ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+
+            if (++projectile.frameCounter >= 4)
+            {
+                projectile.frameCounter = 0;
+                projectile.frame = ++projectile.frame % Main.projFrames[projectile.type];
+            }
+        }
+        public override void Kill(int timeLeft)
+        {
+        
+        }
+    }
+}
