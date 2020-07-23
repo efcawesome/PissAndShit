90211c0397d0a2f29a986e772719faec4d119682
jamespuzzle1 <68666897+jamespuzzle1@users.noreply.github.com>
2020-07-22 22:03:56 -0700

    Update SesameEgg.cs

--- Items/Consumables/SesameEgg.cs
+++ Items/Consumables/SesameEgg.cs
@@ -8,7 +8,7 @@ using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
-namespace CringeMod.Items.Consumables
+namespace PissAndShit.Items.Consumables
 {
     class SesameEgg : ModItem
     {
