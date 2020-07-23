8299752f6c02988192ced906c35f4cceaa805356
BabyblueSheep <47078394+BabyblueSheep@users.noreply.github.com>
2020-07-23 16:49:55 +0300

    Add files via upload
    Bad Time, Kid

---
+++ Buffs/Kamra.cs
@@ -0,0 +1,23 @@
+﻿  using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Buffs
+{
+	public class Kamra : ModBuff
+	{
+        public override void SetDefaults() {
+            DisplayName.SetDefault("Kamra");
+            Description.SetDefault("i'll be 'frank' with you. as much as i like putting hot dogs on your head... thirty is just an excessive number. twenty-nine, now that's fine, but thirty... does it look like my arms can reach that high?");
+			Main.debuff[Type] = true;
+			Main.pvpBuff[Type] = true;
+			Main.buffNoSave[Type] = true;
+			longerExpertDebuff = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex) {
+			player.GetModPlayer<PaSPlayer>().kamra = true;
+		}
+    }
+}
