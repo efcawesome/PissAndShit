f2616cef2325271dc9cede38563048700e19bc87
jamespuzzle1 <68666897+jamespuzzle1@users.noreply.github.com>
2020-07-22 21:56:00 -0700

    fixed bugs
    fixed bugs with god slime

---
+++ NPCs/Bosses/EggBert.cs
@@ -0,0 +1,84 @@
+using PissAndShit.Items.Weapons;
+using Terraria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace PissAndShit.NPCs.Bosses
+{
+    [AutoloadBossHead]
+    public class EggBert : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Egg Bert");
+
+        }
+        private int beeTimer = 0;
+        private int beeType;
+        public override void SetDefaults()
+        {
+            npc.width = 160;
+            npc.height = 200;
+
+            npc.boss = true;
+            npc.aiStyle = 41;
+            aiType = 41;
+
+            npc.npcSlots = 5;
+
+            npc.lifeMax = 2400;
+            npc.damage = 22;
+            npc.defense = 12;
+            npc.knockBackResist = 0f;
+
+            npc.value = Item.buyPrice(gold: 15);
+
+            npc.lavaImmune = false;
+            npc.noTileCollide = false;
+            npc.noGravity = false;
+
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            music = MusicID.LunarBoss;
+
+            bossBag = mod.ItemType("EggBertTreasureBag");
+        }
+        public override void AI()
+        {
+            beeTimer++;
+            if (beeTimer >= 480)
+            {
+                beeTimer = 0;
+                beeType = Main.rand.Next(1);
+                if (!Main.expertMode)
+                {
+                    if (beeType == 0)
+                    {
+                        NPC.NewNPC((int)npc.Center.X + Main.rand.Next(-200, 200), (int)npc.Center.Y + Main.rand.Next(-200, 200), mod.NPCType("Eggling"), npc.whoAmI);
+                    }
+                    if (beeType == 1)
+                    {
+                        NPC.NewNPC((int)npc.Center.X + Main.rand.Next(-200, 200), (int)npc.Center.Y + Main.rand.Next(-200, 200), NPCID.BunnySlimed, npc.whoAmI);
+                    }
+                }
+            }
+        }
+        public override void NPCLoot()
+        {
+            int bossWeapon = Main.rand.Next(2);
+                if (bossWeapon == 0)
+                {
+                    Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("EggK47"));
+                }
+                if (bossWeapon == 1)
+                {
+                    Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("Pan"));
+                }
+        }
+    }
+}
