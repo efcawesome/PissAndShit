9d65f235bae4dfcc17fb15ee16b2bad3706a4f7a
ArnoldPalmergolf <39205784+aadiuchil@users.noreply.github.com>
2020-07-24 10:15:59 -0400

    Fixed namespace error

--- NPCs/Bosses/YoungDuke.cs
+++ NPCs/Bosses/YoungDuke.cs
@@ -5,7 +5,7 @@ using Terraria.ID;
 using Terraria.ModLoader;
 using static Terraria.ModLoader.ModContent;
 
-namespace PissAndShit.NPCs
+namespace PissAndShit.NPCs.Bosses
 {
 	class YoungDuke : ModNPC
 	{
