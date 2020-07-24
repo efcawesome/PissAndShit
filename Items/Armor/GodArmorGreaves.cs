2041f4bfb0a1674378ea53bbc4f94df31e6543af
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-07-23 22:05:20 -0700

    Adds god armor chest+greaves

---
+++ Items/Armor/GodArmorGreaves.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CringeMod.Items.Armor
+{
+    [AutoloadEquip(EquipType.Legs)]
+    class GodArmorGreaves : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Godly Greaves");
+            Tooltip.SetDefault("Increases jump height and speed.\nIncreases movement speed.");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 24;
+            item.height = 24;
+            item.value = Item.sellPrice(gold: 10);
+            item.rare = ItemRarityID.Red;
+            item.defense = 50;
+        }
+
+        public override void UpdateEquip(Player player)
+        {
+            Player.jumpHeight += 12;
+            Player.jumpSpeed *= 3;
+            player.moveSpeed *= 3f;
+        }
+    }
+}
