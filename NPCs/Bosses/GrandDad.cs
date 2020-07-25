bc776468770cb06c625db75001d1617efdaa86b7
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-07-25 12:21:02 -0700

    GRAND DAD

---
+++ NPCs/Bosses/GrandDad.cs
@@ -0,0 +1,147 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Terraria.Localization;
+using System.Security.Policy;
+using System.Data.SqlTypes;
+
+namespace CringeMod.NPCs.Bosses
+{
+    [AutoloadBossHead]
+    class GrandDad : ModNPC
+    {
+        private int frameNum = 0;
+        private bool canTeleport = true;
+        private int teleportTimer = 0;
+        private bool secondPhase = false;
+        private int projectileShoot = 0;
+        private int projectileTimer = 0;
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("GRAND DAD");
+            Main.npcFrameCount[npc.type] = 2;
+        }
+
+        public override void SetDefaults()
+        {
+            npc.width = 122;
+            npc.height = 144;
+
+            npc.boss = true;
+            npc.aiStyle = -1;
+
+            npc.npcSlots = 5;
+
+            npc.lifeMax = 7777777;
+            npc.damage = 500;
+            npc.defense = 777;
+            npc.knockBackResist = 0f;
+
+            npc.value = Item.buyPrice(platinum: 20);
+
+            npc.lavaImmune = true;
+            npc.noTileCollide = true;
+            npc.noGravity = true;
+
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            music = MusicID.Underground;
+
+            bossBag = mod.ItemType("GrandDadBag");
+        }
+
+
+        public override void AI()
+        {
+            Player player = Main.player[npc.target];
+            npc.TargetClosest(true);
+            Vector2 targetPosition = Main.player[npc.target].position;
+            Vector2 target = npc.HasPlayerTarget ? player.Center : Main.npc[npc.target].Center;
+            npc.netAlways = true;
+            if (canTeleport == true)
+            {
+                teleportTimer++;
+            }
+            if (secondPhase == true)
+            {
+                projectileTimer++;
+            }
+            if (npc.life <= npc.lifeMax / 2)
+            {
+                frameNum = 1;
+                secondPhase = true;
+            }
+            if (teleportTimer >= 30)
+            {
+                npc.position.X = targetPosition.X + Main.rand.Next(-500, 500);
+                npc.position.Y = targetPosition.Y + Main.rand.Next(-500, 500);
+                Main.PlaySound(SoundID.Item6);
+                teleportTimer = 0;
+            }
+            if (projectileTimer >= 180)
+            {
+                Vector2 shootPos = npc.Center;
+                float accuracy = 5f * (npc.life / npc.lifeMax);
+                Vector2 shootVel = target - shootPos + new Vector2(Main.rand.NextFloat(-accuracy, accuracy), Main.rand.NextFloat(-accuracy, accuracy));
+                shootVel.Normalize();
+                shootVel *= 14.5f;
+                projectileShoot = Main.rand.Next(3);
+                for (int i = 0; i < (Main.expertMode ? 2 : 1); i++)
+                {
+                    if (projectileShoot == 0)
+                    {
+                        Projectile.NewProjectile(shootPos.X + (float)(-100 * npc.direction) + (float)Main.rand.Next(-20, 20), shootPos.Y - (float)Main.rand.Next(-20, 20), shootVel.X, shootVel.Y, mod.ProjectileType("SevenProj"), npc.damage / 3, 5f);
+                    }
+                    if (projectileShoot == 1)
+                    {
+                        //needs wario apparition
+                    }
+                    if (projectileShoot == 2)
+                    {
+                        //needs personalized copies
+                    }
+                }
+                projectileTimer = 0;
+            }
+            if (player.dead)
+            {
+                canTeleport = false;
+                npc.TargetClosest(false);
+                npc.direction = 1;
+                npc.velocity.Y = npc.velocity.Y - 0.1f;
+                if (npc.timeLeft > 5)
+                {
+                    npc.timeLeft = 5;
+                    return;
+                }
+            }
+
+        }
+
+        public override void FindFrame(int frameHeight)
+        {
+            npc.frame.Y = frameNum * frameHeight;
+        }
+        public override void NPCLoot()
+        {
+            int bossWeapon = Main.rand.Next(4);
+            if(Main.expertMode)
+            {
+                npc.DropBossBags();
+            }
+            else
+            {
+                if (bossWeapon == 0)
+                {
+                    Item.NewItem((int)npc.position.X, (int)npc.position.Y, npc.width, npc.height, mod.ItemType("SevenShortsword"));
+                }
+            }
+        }
+    }
+}
