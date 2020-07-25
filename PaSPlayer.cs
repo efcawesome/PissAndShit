4ac53c93e389cee68cfb13b66c6f41ed6ebe5542
ArnoldPalmergolf <39205784+aadiuchil@users.noreply.github.com>
2020-07-25 13:48:12 -0400

    added soap debuff to mod player

--- PaSPlayer.cs
+++ PaSPlayer.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Xna.Framework;
+using PissAndShit.Dusts;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.GameInput;
@@ -9,16 +10,19 @@ using Terraria.Graphics.Shaders;
 using Terraria.ID;
 using Terraria.Localization;
 using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
 
 namespace PissAndShit
 {
     public class PaSPlayer : ModPlayer
     {
 		public bool kamra = false;
+		public bool soaped = false;
 
         public override void ResetEffects()
         {
             kamra = false;
+			soaped = false;
         }
 
         public override void UpdateBadLifeRegen()
@@ -33,5 +37,23 @@ namespace PissAndShit
 				player.lifeRegen -= 25;
             }
         }
-	}
+
+        public override void DrawEffects(PlayerDrawInfo drawInfo, ref float r, ref float g, ref float b, ref float a, ref bool fullBright)
+        {
+			if (soaped)
+			{
+				if (Main.rand.NextBool(4) && drawInfo.shadow == 0f)
+				{
+					int dust = Dust.NewDust(drawInfo.position - new Vector2(2f, 2f), player.width + 4, player.height + 4, DustType<SoapBubble>(), player.velocity.X * 0.4f, player.velocity.Y * 0.4f, 100, default(Color), 1f);
+					Main.dust[dust].noGravity = true;
+					Main.dust[dust].velocity *= 1.8f;
+					Main.dust[dust].velocity.Y -= 0.5f;
+					Main.playerDrawDust.Add(dust);
+				}
+				r *= 0.1f;
+				g *= 0.5f;
+				b *= 0.7f;
+			}
+		}
+    }
 }
