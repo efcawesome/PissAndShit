3bec6ec254a760633f9bae92640a984fb3d6d9c6
ArnoldPalmergolf <39205784+aadiuchil@users.noreply.github.com>
2020-07-25 13:47:25 -0400

    added main debuff

---
+++ Buffs/Soaped.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+
+namespace PissAndShit.Buffs
+{
+	public class Soaped : ModBuff
+	{
+
+
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Soaped");
+			Description.SetDefault("You are covered in soap");
+			Main.debuff[Type] = true;
+			Main.pvpBuff[Type] = false;
+			Main.buffNoSave[Type] = true;
+			longerExpertDebuff = false;
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			player.GetModPlayer<PaSPlayer>().soaped = true;
+		}
+	}
+}
