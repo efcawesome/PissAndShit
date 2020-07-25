6fd5b5b79f891f35e0c345a688c2322535c2e3d5
tigershark5639 <68576691+tigershark5639@users.noreply.github.com>
2020-07-25 01:39:56 -0400

    Add files via upload

---
+++ Items/Weapons/youngdukeyoyo.cs
@@ -0,0 +1,45 @@
+using PissAndShit.Projectiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using PissAndShit.Projectiles;
+using static Terraria.ModLoader.ModContent;
+
+namespace PissAndShit.Items.Weapons
+{
+    public class youngdukeyoyo : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Hurricane");
+            Tooltip.SetDefault("Incredible Speed and Strength, Also Wet.");
+
+            // These are all related to gamepad controls and don't seem to affect anything else
+            ItemID.Sets.Yoyo[item.type] = true;
+            ItemID.Sets.GamepadExtraRange[item.type] = 15;
+            ItemID.Sets.GamepadSmartQuickReach[item.type] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            item.useStyle = 5;
+            item.width = 26;
+            item.height = 30;
+            item.useAnimation = 25;
+            item.useTime = 20;
+            item.shootSpeed = 16f;
+            item.knockBack = 4.5f;
+            item.damage = 40;
+            item.rare = 3;
+
+            item.melee = true;
+            item.channel = true;
+            item.noMelee = true;
+            item.noUseGraphic = true;
+
+            item.UseSound = SoundID.Item1;
+            item.value = Item.sellPrice(silver: 0);
+            item.shoot = ModContent.ProjectileType<youngdukeyoyoproj>();
+        }
+    }
+}
