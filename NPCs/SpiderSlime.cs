c9343ab573731e2f713a24bb809942648b9528c3
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-07-25 13:55:51 -0700

    spider slime (needs sprite)

---
+++ NPCs/SpiderSlime.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CringeMod.NPCs.Enemies
+{
+    class SpiderSlime : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Spider Slime");
+        }
+
+        public override void SetDefaults()
+        {
+            npc.width = 158;
+            npc.height = 115;
+
+            npc.lifeMax = 55;
+            npc.damage = 20;
+            npc.defense = 5;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            npc.aiStyle = 1;
+            aiType = NPCID.GreenSlime;
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            return SpawnCondition.SpiderCave.Chance * 0.1f;
+        }
+    }
+}
