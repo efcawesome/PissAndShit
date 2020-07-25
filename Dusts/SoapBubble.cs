63ba54bd6306cb299f6652b619a7abb2afda8a05
ArnoldPalmergolf <39205784+aadiuchil@users.noreply.github.com>
2020-07-25 13:46:34 -0400

    Added soap dusts
    dusts

---
+++ Dusts/SoapBubble.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Dusts
+{
+	public class SoapBubble : ModDust
+	{
+		public override void OnSpawn(Dust dust)
+		{
+			dust.noGravity = true;
+			dust.frame = new Rectangle(0, 0, 8, 8);
+			//If our texture had 2 different dust on top of each other (a 30x60 pixel image), we might do this:
+			//dust.frame = new Rectangle(0, Main.rand.Next(2) * 30, 30, 30);
+		}
+
+		public override bool Update(Dust dust)
+		{
+			dust.position += dust.velocity;
+			dust.scale -= 0.01f;
+			if (dust.scale < 0.75f)
+			{
+				dust.active = false;
+			}
+			return false;
+		}
+	}
+}
