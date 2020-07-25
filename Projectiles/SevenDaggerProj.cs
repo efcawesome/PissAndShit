6f960c34cc8181531f6836f667d634bc27b12fb4
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-07-25 12:22:12 -0700

    grand dad proj and op shortsword proj

---
+++ Projectiles/SevenDaggerProj.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CringeMod.Projectiles
+{
+    class SevenDaggerProj : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Shooting Seven");
+        }
+        public override void SetDefaults()
+        {
+            projectile.width = 14;
+            projectile.height = 18;
+            projectile.aiStyle = 1;
+            aiType = ProjectileID.Bullet;
+            projectile.friendly = true;
+            projectile.ignoreWater = true;
+            projectile.penetrate = 1;
+            projectile.timeLeft = 250;
+            projectile.tileCollide = false;
+            projectile.hostile = false;
+            projectile.scale = 1;
+            ProjectileID.Sets.TrailCacheLength[projectile.type] = 5;
+            ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+        }
+
+        public override void AI()
+        {
+            projectile.rotation = (float)Math.Atan2((double)projectile.velocity.Y, (double)projectile.velocity.X) + 1.57f;
+        }
+    }
+}
---
+++ Projectiles/SevenProj.cs
@@ -0,0 +1,39 @@
+﻿using Terraria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace CringeMod.Projectiles
+{
+    class SevenProj : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+			DisplayName.SetDefault("7");
+            Main.projFrames[projectile.type] = 4;
+        }
+        public override void SetDefaults()
+        {
+			projectile.width = 51;
+			projectile.height = 51;
+			projectile.hostile = true;
+			projectile.aiStyle = 1;
+			aiType = ProjectileID.Skull;
+			projectile.ignoreWater = true;
+			projectile.tileCollide = false;
+			projectile.timeLeft = 300;
+			ProjectileID.Sets.TrailCacheLength[projectile.type] = 5;
+			ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+
+			if (++projectile.frameCounter >= 4)
+			{
+				projectile.frameCounter = 0;
+				projectile.frame = ++projectile.frame % Main.projFrames[projectile.type];
+			}
+		}
+    }
+}
