ed472b157a9e41f359d9d5313fea4003294cb180
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-07-25 13:10:46 -0700

    HIVE EXPERT DROP NEEDS SPRITE

---
+++ Items/Accessories/HiveChunk.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CringeMod.Items.Accessories
+{
+    class HiveChunk : ModItem
+    {
+        public override void SetDefaults()
+        {
+            item.width = 32;
+            item.height = 30;
+
+            item.value = Item.buyPrice(gold: 50);
+            item.rare = ItemRarityID.Expert;
+            item.accessory = true;
+            item.expert = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.AddBuff(BuffID.Honey, 10);
+        }
+    }
+}
