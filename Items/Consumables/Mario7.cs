cc1c80ef7120937466b12a482aabee9919d928e5
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-07-25 15:15:12 -0700

    grand dad summon

---
+++ Items/Consumables/Mario7.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CringeMod.Items
+{
+    class Mario7 : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Mario 7");
+            Tooltip.SetDefault("Summons the 7 god himself");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 198;
+            item.height = 51;
+            item.maxStack = 20;
+            item.useStyle = ItemUseStyleID.HoldingUp;
+            item.useTime = 45;
+            item.useAnimation = 45;
+            item.consumable = true;
+            item.rare = ItemRarityID.Red;
+        }
+
+        public override bool CanUseItem(Player player)
+        {
+            return !NPC.AnyNPCs(mod.NPCType("GrandDad"));
+        }
+
+        public override bool UseItem(Player player)
+        {
+            Main.PlaySound(SoundID.Roar, player.position);
+            if (Main.netMode != NetmodeID.MultiplayerClient)
+            {
+                NPC.SpawnOnPlayer(player.whoAmI, mod.NPCType("GrandDad"));
+            }
+            return true;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.LunarBar, 20);
+            recipe.AddTile(TileID.LunarCraftingStation);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
