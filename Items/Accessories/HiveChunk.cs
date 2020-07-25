9e261bdafa7661fe7ffbcbeac121c51d77a2223e
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-07-25 13:54:38 -0700

    made hive chunk usable

--- Items/Accessories/HiveChunk.cs
+++ Items/Accessories/HiveChunk.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -11,10 +11,15 @@ namespace PissAndShit.Items.Accessories
 {
     class HiveChunk : ModItem
     {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Hive Chunk");
+            Tooltip.SetDefault("Gives permanent honey buff\n+250 max life\n+50 defense");
+        }
         public override void SetDefaults()
         {
-            item.width = 26;
-            item.height = 26;
+            item.width = 32;
+            item.height = 30;
 
             item.value = Item.buyPrice(gold: 50);
             item.rare = ItemRarityID.Expert;
@@ -25,6 +30,8 @@ namespace PissAndShit.Items.Accessories
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
             player.AddBuff(BuffID.Honey, 10);
+            player.statLifeMax += 250;
+            player.statDefense += 50;
         }
     }
 }
