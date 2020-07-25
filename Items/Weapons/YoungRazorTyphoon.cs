c32a6a486bfb5334a10a7d87687c639256e5a245
tigershark5639 <68576691+tigershark5639@users.noreply.github.com>
2020-07-25 01:43:05 -0400

    Update YoungRazorTyphoon.cs

--- Items/Weapons/YoungRazorTyphoon.cs
+++ Items/Weapons/YoungRazorTyphoon.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,25 +13,25 @@ namespace PissAndShit.Items.Weapons
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Young Razorblade Typhoon");
+            DisplayName.SetDefault("Whirlpool");
         }
 
         public override void SetDefaults()
         {
-            item.damage = 16;
+            item.damage = 25;
             item.magic = true;
             item.width = 22;
             item.height = 24;
-            item.useTime = 20;
-            item.useAnimation = 40;
+            item.useTime = 15;
+            item.useAnimation = 45;
             item.useStyle = ItemUseStyleID.HoldingOut;
             item.noMelee = true;
             item.knockBack = 0.5f;
             item.value = Item.sellPrice(gold: 1, silver: 50);
             item.rare = ItemRarityID.Green;
-            item.mana = 10;
+            item.mana = 15;
             item.UseSound = SoundID.Item84;
-            item.autoReuse = true;
+            item.autoReuse = false;
             item.shoot = ProjectileID.Typhoon;
             item.shootSpeed = 6;
         }
