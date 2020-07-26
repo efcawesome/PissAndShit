4d64477039435c96402d46316c6a1140c5a4e8c2
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-07-26 01:57:56 -0700

    baby truffle worm (needs sprite idk where it is)

---
+++ NPCs/BabyTruffleWorm.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.NPCs
+{
+    class BabyTruffleWorm : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Truffle Larva");
+        }
+
+        public override void SetDefaults()
+        {
+            npc.width = 158;
+            npc.height = 115;
+
+            npc.lifeMax = 5;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCDeath1;
+            aiType = NPCID.TruffleWorm;
+            npc.aiStyle = 66;
+        }
+
+        public override float SpawnChance(NPCSpawnInfo spawnInfo)
+        {
+            return SpawnCondition.UndergroundMushroom.Chance * 0.1f;
+        }
+    }
+}
