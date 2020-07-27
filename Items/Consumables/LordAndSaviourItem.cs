29a570a39c0b663a161e7aa6edb67457b4aec00f
Exitium <60335462+UmbraExitium@users.noreply.github.com>
2020-07-26 23:11:23 -0300

    Dont question him

---
+++ Items/Consumables/LordAndSaviourItem.cs
@@ -0,0 +1,39 @@
+using Microsoft.Xna.Framework;
+using PissAndShit.NPCs;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace PissAndShit.Items.Consumables
+{
+    public class LordAndSaviourItem : ModItem
+    {
+        public override void SetStaticDefaults(){
+            DisplayName.SetDefault("???");
+            Tooltip.SetDefault("Summons our Lord and Saviour");
+        }
+        public override void SetDefaults()
+        {
+            item.useStyle = 4;
+            item.useTime = 45;
+            item.useAnimation = 45;
+            item.width = 32;
+            item.height = 32;
+            item.rare = -12;
+            item.consumable = true;
+        }
+
+        public override bool UseItem(Player player)
+        {
+            NPC.SpawnOnPlayer(player.whoAmI, NPCType<LordAndSaviour>());
+            Main.PlaySound(SoundID.Roar, player.position, 0);
+            return true;
+        }
+        public override void AddRecipes(){
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
