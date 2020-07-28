0d854f73a36894877ae8147c4e68085d4919fcb1
tigershark5639 <68576691+tigershark5639@users.noreply.github.com>
2020-07-28 17:55:48 -0400

    Add files via upload

---
+++ Items/BigChunk.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Items
+{
+    public class BigChunk : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Big Chunk");
+            Tooltip.SetDefault("large");
+        }
+        public override void SetDefaults()
+        {
+            item.maxStack = 999;
+            item.rare = 6;
+            item.width = 26;
+            item.height = 28;
+            item.scale = 10;
+
+        }
+    }
+}
