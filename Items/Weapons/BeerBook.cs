7aa0954e40a3c772755d441d6d766825b84a3b93
ArnoldPalmergolf <39205784+aadiuchil@users.noreply.github.com>
2020-07-31 16:34:11 -0400

    added the book of beer

---
+++ Items/Weapons/BeerBook.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace PissAndShit.Items.Weapons
+{
+    class BeerBook : ModItem
+
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Beer Book");
+            Tooltip.SetDefault("Use this to make your enemies drunk");
+        }
+
+		public override void SetDefaults()
+		{
+			item.damage = 44;
+			item.width = 15;
+			item.height = 15;
+			item.useTime = 10;
+			item.useAnimation = 10;
+			item.useStyle = ItemUseStyleID.HoldingOut;
+			item.knockBack = 8;
+			item.value = 9200;
+			item.rare = ItemRarityID.Pink;
+			item.UseSound = SoundID.Item1;
+			item.autoReuse = true;
+			item.shoot = mod.ProjectileType("beertwo");
+			item.shootSpeed = 8f;
+		}
+
+		
+
+	}
+}
