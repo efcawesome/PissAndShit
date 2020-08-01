b2fe9aa3cf3486eac6b2d0f907b0057cda53a11e
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-07-31 21:33:54 -0700

    self kil item

---
+++ Items/Misc/PossessedSword.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Items.Misc
+{
+    class PossessedSword : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Sometimes there's only one way out");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 30;
+            item.height = 52;
+            item.maxStack = 1;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+        }
+        public override bool UseItem(Player player)
+        {
+            player.KillMe(PlayerDeathReason.ByCustomReason(player.name + " was bad"), 10000, 1, false);
+            return true;
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.IronBar, 10);
+            recipe.AddIngredient(ItemID.RottenChunk, 10);
+            recipe.AddIngredient(ItemID.GoldCoin, 5);
+            recipe.AddTile(TileID.DemonAltar);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
