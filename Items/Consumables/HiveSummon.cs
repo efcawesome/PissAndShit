6832bcb29b48d3f8b4b2d44570826f64f7fdb2a0
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-07-31 21:56:45 -0700

    hive summon

---
+++ Items/Consumables/HiveSummon.cs
@@ -0,0 +1,53 @@
+﻿using PissAndShit.NPCs.Bosses;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Items.Consumables
+{
+    class HiveSummon : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("oBEEsity");
+            Tooltip.SetDefault("Summons the protector of the bees");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 34;
+            item.height = 34;
+            item.maxStack = 20;
+            item.useStyle = ItemUseStyleID.HoldingUp;
+            item.useTime = 45;
+            item.useAnimation = 45;
+            item.consumable = true;
+            item.rare = ItemRarityID.Red;
+        }
+        public override bool CanUseItem(Player player)
+        {
+            return !NPC.AnyNPCs(ModContent.NPCType<Hive>());
+        }
+
+        public override bool UseItem(Player player)
+        {
+            Main.PlaySound(SoundID.Roar, player.position);
+            if (Main.netMode != NetmodeID.MultiplayerClient)
+            {
+                NPC.SpawnOnPlayer(player.whoAmI, ModContent.NPCType<Hive>());
+            }
+            return true;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.Hive, 50);
+            recipe.AddIngredient(ItemID.FragmentNebula, 10);
+            recipe.AddIngredient(ItemID.FragmentSolar, 10);
+            recipe.AddIngredient(ItemID.FragmentStardust, 10);
+            recipe.AddIngredient(ItemID.FragmentVortex, 10);
+            recipe.AddTile(TileID.DemonAltar);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
