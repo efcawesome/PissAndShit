11193cc790810af0f5dad27adf02c572e3606191
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-08-03 20:03:55 -0700

    better recipe

--- Items/Armor/FesteringBreastPlate.cs
+++ Items/Armor/FesteringBreastPlate.cs
@@ -1,4 +1,4 @@
-﻿using Terraria;
+using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
 
@@ -28,6 +28,18 @@ namespace PissAndShit.Items.Armor
         {
             player.statDefense += 150;
             player.endurance += 130;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.SolarFlareBreastplate, 1);
+            recipe.AddIngredient(ItemID.NebulaBreastplate, 1);
+            recipe.AddIngredient(ItemID.StardustBreastplate, 1);
+            recipe.AddIngredient(ItemID.VortexBreastplate, 1);
+            recipe.AddIngredient(ItemID.LunarBar, 100);
+            recipe.AddTile(TileID.LunarCraftingStation);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
         }
     }
 }
