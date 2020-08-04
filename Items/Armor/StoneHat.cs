f9edb5515dbfe49cdc1cc039b2260919c460b892
tigershark5639 <68576691+tigershark5639@users.noreply.github.com>
2020-08-03 22:51:36 -0400

    Add files via upload

---
+++ Items/Armor/StoneHat.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+
+namespace PissAndShit.Items.Armor
+{
+	[AutoloadEquip(EquipType.Head)]
+	public class StoneHat : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			Tooltip.SetDefault("Hat.");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 24;
+			item.height = 22;
+			item.value = 10000;
+			item.rare = 5;
+			item.defense = 25;
+		}
+		public override bool IsArmorSet(Item head, Item body, Item legs)
+		{
+			return head.type == ItemType<StoneHat>();
+		}
+
+		public override void UpdateArmorSet(Player player)
+		{
+			player.setBonus = "Stone";
+			player.maxRunSpeed = 0;
+			player.runAcceleration = 0;
+			player.wingTimeMax = 0;
+			player.lifeRegen = 100;
+			player.allDamage = 0.5f;
+
+		}
+
+		public override void AddRecipes()
+		{
+			ModRecipe recipe = new ModRecipe(mod);
+			recipe.AddIngredient(ItemType<StoneBar>(), 10);
+			recipe.AddTile(TileID.Anvils);
+			recipe.SetResult(this);
+			recipe.AddRecipe();
+		}
+
+	}
+}
+
