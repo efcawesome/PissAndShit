2823abd4a6c6b042e36e919ea9f59564d82eec69
tigershark5639 <68576691+tigershark5639@users.noreply.github.com>
2020-08-03 22:50:55 -0400

    Add files via upload

---
+++ Items/StoneBar.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Items
+{
+   public class StoneBar: ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Stone bar");
+            Tooltip.SetDefault("bar");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 30;
+            item.height = 24;
+            item.maxStack = 999;
+            item.rare = 2;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.StoneBlock, 20);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
