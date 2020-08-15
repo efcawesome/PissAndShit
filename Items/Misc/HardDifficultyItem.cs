af5405eb1a3a6163043977f8e16a22abae40bf02
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-08-15 13:56:56 -0700

    second part of good difficulty

---
+++ Items/Misc/HardDifficultyItem.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Xna.Framework;
+using PissAndShit.NPCs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Items.Misc
+{
+    class HardDifficultyItem : ModItem
+    {
+        private static bool difficultyActive = false;
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Super True Expert Masochist Eternity Revengance Death mode");
+            Tooltip.SetDefault("Have fun!");
+        }
+        public override void SetDefaults()
+        {
+            item.width = 34;
+            item.height = 34;
+            item.maxStack = 1;
+            item.useStyle = ItemUseStyleID.HoldingUp;
+            item.useTime = 45;
+            item.useAnimation = 45;
+            item.consumable = false;
+            item.rare = ItemRarityID.Red;
+        }
+        public override bool UseItem(Player player)
+        {
+            if (difficultyActive == false)
+            {
+                AGLobalNPC.hardDifficulty = true;
+                difficultyActive = true;
+                Main.NewText("GET READY FOR A CHALLENGE", 135, 16, 22);
+            }
+            else
+            {
+                AGLobalNPC.hardDifficulty = false;
+                difficultyActive = false;
+                Main.NewText("noob", 48, 248, 255);
+            }
+            return true;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddTile(TileID.DemonAltar);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
