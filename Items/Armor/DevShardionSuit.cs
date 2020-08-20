67d57a43bf4b5881f9cee9a8fcf9ad775baf747d
Shardion <ShardionsSink@protonmail.com>
2020-08-19 17:03:50 -0700

    stop deving dev sets to fix death not finding bomb

---
+++ Items/Armor/DevShardionSuit.cs
@@ -0,0 +1,23 @@
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Items.Armor
+{
+    [AutoloadEquip(EquipType.Body)]
+    public class DevShardionSuit : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Shardion's Scorching Suit");
+	    Tooltip.SetDefault("'Imagine having power over Death while wearing this'");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 30;
+            item.height = 18;
+            item.vanity = true;
+            item.rare = -12;
+        }
+    }
+}
