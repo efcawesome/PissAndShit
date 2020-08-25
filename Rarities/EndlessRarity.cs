ea1be5a46885cc085361ebfd9b1383224c070138
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-08-24 20:43:52 -0700

    rarity pt. 4

---
+++ Rarities/EndlessRarity.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TomatoLib.Core.Utils.API;
+
+namespace PissAndShit.Rarities
+{
+    public class EndlessRarity : ModRarity
+    {
+        public override Color RarityColor() => new Color(69, 69, 69);
+    }
+}
