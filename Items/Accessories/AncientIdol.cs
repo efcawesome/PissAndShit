8f1d35b9a9dbeb9928c138cc156e0cb3d39756b3
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-08-24 20:44:28 -0700

    ancient idol (custom rarity item)

---
+++ Items/Accessories/AncientIdol.cs
@@ -0,0 +1,30 @@
+﻿using PissAndShit.Rarities;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Items.Accessories
+{
+    class AncientIdol : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Shoot golem beams after taking damage");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 32;
+            item.height = 30;
+
+            item.value = Item.buyPrice(gold: 50);
+            item.rare = TomatoLib.Core.TomatoLoader.RarityType<EndlessRarity>();
+            item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            player.GetModPlayer<PaSPlayer>().ancientIdol = true;
+        }
+    }
+}
