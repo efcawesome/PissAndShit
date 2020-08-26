e6f2e750488c2a009450762e0fa4e2cb11a64111
ArnoldPalmergolf <39205784+ArnoldPalmerGolf@users.noreply.github.com>
2020-08-26 19:26:37 -0400

    added grand dad brick

---
+++ Placeables/GrandBrickItem.cs
@@ -0,0 +1,37 @@
+﻿using PissAndShit.Tiles;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace PissAndShit.Items.Placeables
+{
+    public class GrandBrickItem : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Grand Brick");
+            Tooltip.SetDefault("");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 12;
+            item.height = 12;
+            item.maxStack = 999;
+            item.useTurn = true;
+            item.autoReuse = true;
+            item.useAnimation = 15;
+            item.useTime = 10;
+            item.useStyle = ItemUseStyleID.SwingThrow;
+            item.consumable = true;
+            item.createTile = TileType<GrandBrick>();
+            // Set other item.X values here
+        }
+
+        public override void AddRecipes()
+        {
+            // Recipes here. See Basic Recipe Guide
+        }
+    }
+}
---
+++ Tiles/GrandBrick.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Xna.Framework;
+using PissAndShit.Items.Placeables;
+using Steamworks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Tiles
+{
+	public class GrandBrick : ModTile
+	{
+		public override void SetDefaults()
+		{
+			Main.tileSolid[Type] = true;
+			Main.tileBlockLight[Type] = true;
+			Main.tileLighted[Type] = true;;
+			Main.tileBrick[Type] = true;
+			dustType = DustID.BlueCrystalShard;
+			drop = ModContent.ItemType<GrandBrickItem>();
+			AddMapEntry(new Color(140, 184, 255));
+			// Set other values here
+		}
+	}
+}
