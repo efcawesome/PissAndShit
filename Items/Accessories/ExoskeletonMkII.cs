22a3ceae925abe12ce6f769383a2cb8251c672ab
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-08-27 02:46:27 -0700

    exoskeletons

--- Items/Accessories/ExoskeletonMkII.cs
+++ Items/Accessories/ExoskeletonMkII.cs
@@ -4,18 +4,18 @@ using Terraria.ModLoader;
 
 namespace PissAndShit.Items.Accessories
 {
-    public class Exoskeleton : ModItem
+    public class ExoskeletonMkII : ModItem
     {
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("Exoskeleton");
+            DisplayName.SetDefault("Exoskeleton Mk. II");
             Tooltip.SetDefault("20 defense\n10% increased damage reduction\nEndless Drop");
         }
 
         public override void SetDefaults()
         {
             item.width = 32;
-            item.height = 18;
+            item.height = 20;
             item.rare = ItemRarityID.Expert;
 
             item.value = Item.buyPrice(gold: 50);
@@ -24,9 +24,9 @@ namespace PissAndShit.Items.Accessories
 
         public override void UpdateAccessory(Player player, bool hideVisual)
         {
-            player.statDefense += 20;
-            player.endurance += 0.1f;
-            player.GetModPlayer<PaSPlayer>().exoskeletonBad = true;
+            player.statDefense += 35;
+            player.endurance += 0.2f;
+            player.GetModPlayer<PaSPlayer>().exoskeletonGood = true;
         }
     }
 }
