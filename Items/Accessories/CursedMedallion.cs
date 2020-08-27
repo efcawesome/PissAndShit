792f6fb1dbc0a1c549f7b67ee5a4ad6eba0fd5ea
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-08-26 22:00:45 -0700

    Add files via upload

---
+++ Items/Accessories/CursedMedallion.cs
@@ -0,0 +1,28 @@
+﻿using PissAndShit.NPCs;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Items.Accessories
+{
+    public class CursedMedallion : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("Cursed souls have a chance to spawn on enemy deaths\nWhen killed you regenerate 25 life");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 48;
+            item.height = 56;
+
+            item.value = Item.buyPrice(gold: 15);
+            item.accessory = true;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            PaSGlobalNPC.cursedMedallionSpawn = player.GetModPlayer<PaSPlayer>().cursedMedallion = true;
+        }
+    }
+}
