cf4e97becfd2c1617ae54e5190bd87609c30b274
ArnoldPalmergolf <39205784+ArnoldPalmerGolf@users.noreply.github.com>
2020-08-28 17:48:21 -0400

    Add files via upload

---
+++ NPCs/WarioApperition.cs
@@ -0,0 +1,24 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.NPCs.Bosses
+{
+	class WarioApperition : ModNPC
+	{
+
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("WarioApperition");
+			Main.npcFrameCount[npc.type] = Main.npcFrameCount[NPCID.DungeonGuardian];
+		}
+
+		public override void SetDefaults()
+		{
+			npc.CloneDefaults(NPCID.DungeonGuardian);
+			aiType = NPCID.DungeonGuardian;
+			animationType = NPCID.DungeonGuardian;
+		}
+	}
+}
