877d0a42a970ca0eaf3950e2a433c2c615b0f3e2
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-08-29 15:59:21 -0700

    DAEDALUS CLASS

---
+++ Items/DaedalusDamage/DaedalusDamageItem.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Items.DaedalusDamage
+{
+    public abstract class DaedalusDamageItem : ModItem
+    {
+        public override bool CloneNewInstances => true;
+        public virtual void SafeSetDefaults()
+        {
+
+        }
+		public sealed override void SetDefaults()
+		{
+			SafeSetDefaults();
+			item.melee = false;
+			item.ranged = false;
+			item.magic = false;
+			item.thrown = false;
+			item.summon = false;
+		}
+		public override void ModifyWeaponDamage(Player player, ref float add, ref float mult, ref float flat)
+		{
+			add += DaedalusDamagePlayer.ModPlayer(player).daedalusDamageAdd;
+			mult *= DaedalusDamagePlayer.ModPlayer(player).daedalusDamageMult;
+		}
+
+		public override void GetWeaponKnockback(Player player, ref float knockback)
+		{
+			knockback += DaedalusDamagePlayer.ModPlayer(player).daedalusKnockback;
+		}
+
+		public override void GetWeaponCrit(Player player, ref int crit)
+		{
+			crit += DaedalusDamagePlayer.ModPlayer(player).daedalusCrit;
+		}
+
+		public override void ModifyTooltips(List<TooltipLine> tooltips)
+		{
+			TooltipLine tt = tooltips.FirstOrDefault(x => x.Name == "Damage" && x.mod == "Terraria");
+			if (tt != null)
+			{
+				string[] splitText = tt.text.Split(' ');
+				string damageValue = splitText.First();
+				string damageWord = splitText.Last();
+				tt.text = damageValue + " daedalus " + damageWord;
+			}
+		}
+	}
+}
---
+++ Items/DaedalusDamage/DaedalusDamagePlayer.cs
@@ -0,0 +1,36 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Items.DaedalusDamage
+{
+    class DaedalusDamagePlayer : ModPlayer
+    {
+        public static DaedalusDamagePlayer ModPlayer(Player player)
+        {
+            return player.GetModPlayer<DaedalusDamagePlayer>();
+        }
+
+        public float daedalusDamageAdd;
+		public float daedalusDamageMult = 1f;
+        public float daedalusKnockback;
+        public int daedalusCrit;
+
+		public override void ResetEffects()
+		{
+			ResetVariables();
+		}
+
+		public override void UpdateDead()
+		{
+			ResetVariables();
+		}
+
+        private void ResetVariables()
+        {
+            daedalusDamageAdd = 0f;
+            daedalusDamageMult = 1f;
+            daedalusKnockback = 0f;
+            daedalusCrit = 0;
+        }
+	}
+}
