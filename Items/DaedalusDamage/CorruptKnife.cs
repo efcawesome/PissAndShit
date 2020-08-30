4eb3d2e3752e451397c738fb3e00e1edda7679c7
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-08-29 18:05:50 -0700

    god

--- Items/DaedalusDamage/CorruptKnife.cs
+++ Items/DaedalusDamage/CorruptKnife.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Xna.Framework;
+using PissAndShit.Projectiles;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,32 +11,32 @@ using Terraria.ModLoader;
 
 namespace PissAndShit.Items.DaedalusDamage
 {
-    class NebulaRain : DaedalusDamageItem
+    class CorruptKnife : DaedalusDamageItem
     {
         public override void SetStaticDefaults()
         {
-            Tooltip.SetDefault("Lasers from the sky");
+            Tooltip.SetDefault("This blade has been corrupted beyond repair");
         }
         public override void SafeSetDefaults()
         {
-            item.width = 99;
-            item.height = 99;
-            item.useTime = 19;
-            item.useAnimation = 19;
-            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.width = 24;
+            item.height = 24;
+            item.useTime = 23;
+            item.useAnimation = 23;
+            item.useStyle = ItemUseStyleID.SwingThrow;
             item.noMelee = true;
-            item.damage = 100;
+            item.damage = 25;
             item.knockBack = 1.2f;
             item.maxStack = 1;
-            item.shoot = ProjectileID.NebulaLaser;
-            item.shootSpeed = 10.25f;
-            item.rare = ItemRarityID.Red;
+            item.shoot = ModContent.ProjectileType<CorruptKnifeProj>();
+            item.noUseGraphic = true;
+            item.shootSpeed = 10.5f;
+            item.rare = ItemRarityID.Green;
             item.crit = 4;
-            item.autoReuse = true;
         }
         public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
         {
-            int numberProjectiles = 6 + Main.rand.Next(4);
+            int numberProjectiles = 2 + Main.rand.Next(2);
             for (int index = 0; index < numberProjectiles; ++index)
             {
                 Vector2 vector2_1 = new Vector2((float)(player.position.X + player.width * 0.5 + Main.rand.Next(201) * -player.direction + (Main.mouseX + (double)Main.screenPosition.X - player.position.X)), (float)(player.position.Y + player.height * 0.5 - 600.0));
@@ -57,19 +58,17 @@ namespace PissAndShit.Items.DaedalusDamage
                 float num15 = item.shootSpeed / num14;
                 float num16 = num12 * num15;
                 float num17 = num13 * num15;
-                float SpeedX = num16 + Main.rand.Next(-30, 31) * 0.02f;
+                float SpeedX = num16 + Main.rand.Next(-5, 6) * 0.02f;
                 float SpeedY = num17 + Main.rand.Next(-15, 16) * 0.02f;
-                int projectile = Projectile.NewProjectile(vector2_1.X, vector2_1.Y, SpeedX, SpeedY, type, damage, knockBack, Main.myPlayer, 0.0f, Main.rand.Next(5));
-                Main.projectile[projectile].friendly = true;
-                Main.projectile[projectile].hostile = false;
+                Projectile.NewProjectile(vector2_1.X, vector2_1.Y, SpeedX, SpeedY, type, damage, knockBack, Main.myPlayer, 0.0f, Main.rand.Next(5));
             }
             return false;
         }
         public override void AddRecipes()
         {
             ModRecipe recipe = new ModRecipe(mod);
-            recipe.AddIngredient(ItemID.NebulaBrainBanner, 1);
-            recipe.AddIngredient(ItemID.FragmentNebula, 18);
+            recipe.AddIngredient(ItemID.PoisonedKnife, 100);
+            recipe.AddIngredient(ItemID.DemoniteBar, 15);
             recipe.AddTile(TileID.Anvils);
             recipe.SetResult(this);
             recipe.AddRecipe();
---
+++ Items/DaedalusDamage/CorruptKnifeProj.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Items.DaedalusDamage
+{
+    public class CorruptKnifeProj : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Corrupt Knife");
+            ProjectileID.Sets.TrailCacheLength[projectile.type] = 5;
+            ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 24;
+            projectile.height = 24;
+            projectile.aiStyle = ProjectileID.WoodenArrowFriendly;
+            projectile.friendly = true;
+            projectile.ignoreWater = true;
+            projectile.penetrate = 1;
+            projectile.timeLeft = 500;
+            projectile.tileCollide = true;
+            projectile.hostile = false;
+            projectile.scale = 1;
+            aiType = ProjectileID.WoodenArrowFriendly;
+        }
+
+        public override void AI() 
+        {
+            projectile.rotation = (float)Math.Atan2(projectile.velocity.Y, projectile.velocity.X) + 1.57f;
+            Dust dust;
+            Vector2 position = projectile.Center;
+            dust = Main.dust[Terraria.Dust.NewDust(position, 30, 30, 15, 0f, 0f, 0, new Color(184, 0, 255), 1f)];
+
+        }
+        public override void Kill(int timeLeft)
+        {
+            Collision.HitTiles(projectile.position, projectile.velocity, projectile.width, projectile.height);
+            Main.PlaySound(SoundID.Item10, projectile.position);
+        }
+    }
+}
