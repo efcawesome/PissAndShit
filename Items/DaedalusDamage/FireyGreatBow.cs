4659886684e3c0a77bd53794aa5e9e5a089caf1e
ChiknNuget <70539405+ChiknNuget@users.noreply.github.com>
2020-09-02 18:53:24 -0400

    Add files via upload

---
+++ Items/DaedalusDamage/FireyGreatBow.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Items.DaedalusDamage
+{
+    class FieryGreatStormbow : DaedalusDamageItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("spahghetti");
+        }
+        public override void SafeSetDefaults()
+        {
+            item.width = 50;
+            item.height = 50;
+            item.useTime = 22;
+            item.useAnimation = 22;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.value = Item.sellPrice(gold: 2);
+            item.UseSound = SoundID.Item5;
+            item.noMelee = true;
+            item.damage = 45;
+            item.knockBack = 10f;
+            item.maxStack = 1;
+            item.shoot = ProjectileID.DD2FlameBurstTowerT1Shot;
+            item.shootSpeed = 15f;
+            item.rare = ItemRarityID.Green;
+            item.crit = 4;
+            item.autoReuse = false;
+        }
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            int numberProjectiles = 1 + Main.rand.Next(1);
+            for (int index = 0; index < numberProjectiles; ++index)
+            {
+                Vector2 vector2_1 = new Vector2((float)(player.position.X + player.width * 0.5 + Main.rand.Next(201) * -player.direction + (Main.mouseX + (double)Main.screenPosition.X - player.position.X)), (float)(player.position.Y + player.height * 0.5 - 600.0));
+                vector2_1.X = (float)((vector2_1.X + (double)player.Center.X) / 2.0) + Main.rand.Next(-200, 201);
+                vector2_1.Y -= 100 * index;
+                float num12 = Main.mouseX + Main.screenPosition.X - vector2_1.X;
+                float num13 = Main.mouseY + Main.screenPosition.Y - vector2_1.Y;
+                if (num13 < 0.0)
+                {
+                    num13 *= -1f;
+                }
+
+                if (num13 < 20.0)
+                {
+                    num13 = 20f;
+                }
+
+                float num14 = (float)Math.Sqrt(num12 * (double)num12 + num13 * (double)num13);
+                float num15 = item.shootSpeed / num14;
+                float num16 = num12 * num15;
+                float num17 = num13 * num15;
+                float SpeedX = num16 + Main.rand.Next(-5, 6) * 0.02f;
+                float SpeedY = num17 + Main.rand.Next(-5, 6) * 0.02f;
+                Projectile.NewProjectile(vector2_1.X, vector2_1.Y, SpeedX, SpeedY, type, damage, knockBack, Main.myPlayer, 0.0f, Main.rand.Next(5));
+            }
+            return false;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.DD2FlameburstTowerT1Popper, 2);
+            recipe.AddIngredient(ItemID.HellstoneBar, 18);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
