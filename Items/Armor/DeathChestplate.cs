0130191285e37ef811a936d87fcbf12f753efacb
Shardion <ShardionsSink@protonmail.com>
2020-09-02 00:10:07 -0700

    death armor, currently money armor copy

---
+++ Items/Armor/DeathChestplate.cs
@@ -0,0 +1,39 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.ModLoader.ModContent;
+
+namespace PissAndShit.Items.Armor
+{
+    [AutoloadEquip(EquipType.Body)]
+    public class DeathChestplate : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Death Chainmail");
+        }
+
+        public override void SetDefaults()
+        {
+            item.width = 30;
+            item.height = 20;
+            item.value = 10000;
+            item.rare = ItemRarityID.Lime;
+            item.defense = 40;
+        }
+
+        public override bool IsArmorSet(Item head, Item body, Item legs)
+        {
+            return body.type == ItemType<DeathChestplate>() && legs.type == ItemType<DeathGreaves>();
+        }
+
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemType<DeathMetal>(), 45);
+            recipe.AddTile(TileID.LunarCraftingStation);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
