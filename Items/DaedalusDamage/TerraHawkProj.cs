03e56113b0ee228c97135b75408acb409dedecc4
efcawesome <68629138+efcawesome@users.noreply.github.com>
2020-09-03 11:27:41 -0700

    very cool stuff

---
+++ Items/DaedalusDamage/TerraHawkProj.cs
@@ -0,0 +1,60 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Items.DaedalusDamage
+{
+    public class TerraHawkProj : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Terra Hawk");
+            ProjectileID.Sets.TrailCacheLength[projectile.type] = 5;
+            ProjectileID.Sets.TrailingMode[projectile.type] = 0;
+            Main.projFrames[projectile.type] = 4;
+        }
+
+        public override void SetDefaults()
+        {
+            projectile.width = 133;
+            projectile.height = 45;
+            projectile.aiStyle = -1;
+            projectile.friendly = true;
+            projectile.ignoreWater = true;
+            projectile.penetrate = 1;
+            projectile.timeLeft = 500;
+            projectile.tileCollide = false;
+            projectile.hostile = false;
+            projectile.scale = 1;
+            aiType = -1;
+        }
+
+        public override void AI() 
+        {
+            if(projectile.velocity.X < 6)
+            {
+                projectile.velocity.X += 0.2f;
+            }
+            projectile.velocity.Y = 0;
+            Dust dust;
+            Vector2 position = projectile.Center;
+            dust = Main.dust[Terraria.Dust.NewDust(position, 133, 45, 15, 0f, 0f, 0, new Color(58, 255, 28), 1f)];
+            projectile.ai[0]++;
+            if(projectile.ai[0] >= 30)
+            {
+                Projectile.NewProjectile(projectile.Center, new Vector2(0, 10), ModContent.ProjectileType<TerraHawkBomb>(), 100, 5, Main.myPlayer);
+                projectile.ai[0] = 0;
+            }
+            if (++projectile.frameCounter >= 4)
+            {
+                projectile.frameCounter = 0;
+                if (++projectile.frame >= 4)
+                {
+                    projectile.frame = 0;
+                }
+            }
+        }
+    }
+}
---
+++ Items/DaedalusDamage/VoreBow.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.Items.DaedalusDamage
+{
+    class VoreBow : DaedalusDamageItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Tooltip.SetDefault("VORE");
+        }
+        public override void SafeSetDefaults()
+        {
+            item.width = 29;
+            item.height = 91;
+            item.useTime = 17;
+            item.useAnimation = 17;
+            item.useStyle = ItemUseStyleID.HoldingOut;
+            item.noMelee = true;
+            item.damage = 150;
+            item.knockBack = 1.2f;
+            item.maxStack = 1;
+            item.shoot = ProjectileID.VortexBeaterRocket;
+            item.shootSpeed = 10.25f;
+            item.rare = ItemRarityID.Red;
+            item.crit = 4;
+        }
+        public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+        {
+            int numberProjectiles = 6 + Main.rand.Next(4);
+            for (int index = 0; index < numberProjectiles; ++index)
+            {
+                Vector2 vector2_1 = new Vector2((float)(player.position.X + player.width * 0.5 + Main.rand.Next(201) * -player.direction + (Main.mouseX + (double)Main.screenPosition.X - player.position.X)), (float)(player.position.Y + player.height * 0.5 - 600.0));
+                vector2_1.X = (float)((vector2_1.X + (double)player.Center.X) / 2.0) + Main.rand.Next(-200, 201);
+                vector2_1.Y -= 100 * index;
+                float num12 = Main.mouseX + Main.screenPosition.X - vector2_1.X;
+                float num13 = Main.mouseY + Main.screenPosition.Y - vector2_1.Y;
+                if (num13 < 0.0)
+                {
+                    num13 *= -1f;
+                }
+
+                if (num13 < 20.0)
+                {
+                    num13 = 20f;
+                }
+
+                float num14 = (float)Math.Sqrt(num12 * (double)num12 + num13 * (double)num13);
+                float num15 = item.shootSpeed / num14;
+                float num16 = num12 * num15;
+                float num17 = num13 * num15;
+                float SpeedX = num16 + Main.rand.Next(-5, 6) * 0.02f;
+                float SpeedY = num17 + Main.rand.Next(-5, 6) * 0.02f;
+                Projectile.NewProjectile(vector2_1.X, vector2_1.Y, SpeedX, SpeedY, type, damage, knockBack, Main.myPlayer, 0.0f, Main.rand.Next(5));
+            }
+            return false;
+        }
+        public override void AddRecipes()
+        {
+            ModRecipe recipe = new ModRecipe(mod);
+            recipe.AddIngredient(ItemID.VortexBeater, 1);
+            recipe.AddIngredient(ItemID.FragmentSolar, 10);
+            recipe.AddTile(TileID.Anvils);
+            recipe.SetResult(this);
+            recipe.AddRecipe();
+        }
+    }
+}
