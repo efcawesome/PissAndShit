2e608123205967cc59558c898158906cb05dcf2e
Shardion <ShardionsSink@protonmail.com>
2020-09-08 09:35:02 -0700

    finally update bchecklist calls + fix dum

--- PissAndShit.cs
+++ PissAndShit.cs
@@ -19,7 +19,7 @@ namespace PissAndShit
                 AddMusicBox(GetSoundSlot(SoundType.Music, "Sounds/Music/heavenly_bullshit"), ItemType("GodSlimeMusicBox"), TileType("GodSlimeMusicBox"));
                 AddMusicBox(GetSoundSlot(SoundType.Music, "Sounds/Music/YungDook_2"), ItemType("YoungDukeMusicBox"), TileType("YoungDukeMusicBox"));
                 AddMusicBox(GetSoundSlot(SoundType.Music, "Sounds/Music/GRANDDAD"), ItemType("GrandDadMusicBox"), TileType("GrandDadMusicBox"));
-		AddMusicBox(GetSoundSlot(SoundType.Music, "Sounds/Music/Staying_As_a_1.14"), ItemType("BoozeshrumeMusicBox"), TileType("BoozeshrumeMusicBoxSheet"));
+		        AddMusicBox(GetSoundSlot(SoundType.Music, "Sounds/Music/Staying_As_a_1.14"), ItemType("BoozeshrumeMusicBox"), TileType("BoozeshrumeMusicBoxSheet"));
             }
 
         }
@@ -30,62 +30,70 @@ namespace PissAndShit
             if (bossChecklist != null)
             {
                 bossChecklist.Call(
-                        "AddBoss",
-			5.5f,
-			ModContent.NPCType<NPCs.Bosses.YoungDuke>(),
-			this,
-			"Young Duke",
-                        (Func<bool>)(() => PaSWorld.downedYoungDuke),
-			ModContent.ItemType<YoungWorm>(),
-			new List<int> { ModContent.ItemType<Items.Misc.YoungDukeMusicBox>() },
-			new List<int> { ModContent.ItemType<Items.Weapons.YoungBow>(), ModContent.ItemType<Items.Weapons.YoungGun>(), ModContent.ItemType<Items.Weapons.youngdukeyoyo>(), ModContent.ItemType<Items.Weapons.YoungRazorTyphoon>() },
-			"Spawn by using [i:" + ModContent.ItemType<YoungWorm>() + "] at the Ocean.",
-			"Young Duke has outsped everyone!",
-			"PissAndShit/NPCs/Bosses/YoungDuke_Still",
-			"PissAndShit/NPCs/Bosses/YoungDuke_Head_Boss"
-                    );
+                "AddBoss",
+			    5.5f,
+			    ModContent.NPCType<NPCs.Bosses.YoungDuke>(),
+			    this,
+			    "Young Duke",
+                (Func<bool>)(() => PaSWorld.downedYoungDuke),
+			    ModContent.ItemType<YoungWorm>(),
+			    new List<int> { ModContent.ItemType<Items.Misc.YoungDukeMusicBox>() },
+			    new List<int> { ModContent.ItemType<Items.Weapons.YoungBow>(), ModContent.ItemType<Items.Weapons.YoungGun>(), ModContent.ItemType<Items.Weapons.youngdukeyoyo>(), ModContent.ItemType<Items.Weapons.YoungRazorTyphoon>() },
+			    "Spawn by using [i:" + ModContent.ItemType<YoungWorm>() + "] at the Ocean.",
+			    "Young Duke has outsped everyone!",
+			    "PissAndShit/NPCs/Bosses/YoungDuke_Still",
+			    "PissAndShit/NPCs/Bosses/YoungDuke_Head_Boss"
+                );
 		
-            bossChecklist.Call(
-            "AddBoss",
-			6.5f,
-			ModContent.NPCType<NPCs.Bosses.boozeshrume>(),
-			this,
-			"boozeshrume.exe",
-                        (Func<bool>)(() => PaSWorld.downedBoozeshrume),
-			ModContent.ItemType<SuspiciousAle>(),
-			new List<int> { ModContent.ItemType<Items.Misc.BoozeshrumeMusicBox>() },
-			new List<int> { ModContent.ItemType<Items.BossBags.BoozeshrumeBag>(), ModContent.ItemType<Items.Misc.ScrumpyCiderRedwineTequillaWhiskeyVodkaRumArrackSpiritPureEthanolDrinkMix>(), ModContent.ItemType<Items.Weapons.BeerBook>(), ModContent.ItemType<Items.Weapons.BeerBow>() },
-			"Spawn by using [i:" + ModContent.ItemType<SuspiciousAle>() + "].",
-			"boozeshrume.exe has completed successfully.",
-			"PissAndShit/NPCs/Bosses/boozeshrume",
-			"PissAndShit/NPCs/Bosses/boozeshrume_Head_Boss"
-             );
-		// TODO: These calls are deprecated! Replace with 1.0 AddBoss before release!
-		/*
-                BossChecklist.Call(
-                        "AddBossWithInfo",
-                        "GRAND DAD",
-                        19f,
-                        (Func<bool>)(() => PaSWorld.downedGrandDad),
-                        "Use a [i:" + ModContent.ItemType<Mario7>() + "]"
-                    );
-                BossChecklist.Call(
-                        "AddBossWithInfo",
-                        "Boozeshrume.exe",
-                        6.5f,
-                        (Func<bool>)(() => PaSWorld.downedBoozeshrume),
-                        "Use a [i:" + ModContent.ItemType<SuspiciousAle>() + "]"
-                    );
-                BossChecklist.Call(
-                        "AddBossWithInfo",
-                        "The Hive",
-                        18f,
-                        (Func<bool>)(() => PaSWorld.downedHive),
-                        "Use an [i:" + ModContent.ItemType<HiveSummon>() + "]"
-                    );
-*/
+                bossChecklist.Call(
+                "AddBoss",
+			    6.5f,
+			    ModContent.NPCType<NPCs.Bosses.boozeshrume>(),
+			    this,
+			    "boozeshrume.exe",
+                (Func<bool>)(() => PaSWorld.downedBoozeshrume),
+			    ModContent.ItemType<SuspiciousAle>(),
+			    new List<int> { ModContent.ItemType<Items.Misc.BoozeshrumeMusicBox>() },
+			    new List<int> { ModContent.ItemType<Items.BossBags.BoozeshrumeBag>(), ModContent.ItemType<Items.Misc.ScrumpyCiderRedwineTequillaWhiskeyVodkaRumArrackSpiritPureEthanolDrinkMix>(), ModContent.ItemType<Items.Weapons.BeerBook>(), ModContent.ItemType<Items.Weapons.BeerBow>() },
+			    "Spawn by using [i:" + ModContent.ItemType<SuspiciousAle>() + "].",
+			    "boozeshrume.exe has completed successfully.",
+			    "PissAndShit/NPCs/Bosses/boozeshrume",
+			    "PissAndShit/NPCs/Bosses/boozeshrume_Head_Boss"
+                );
+
+                bossChecklist.Call(
+                "AddBoss",
+			    6.5f,
+			    ModContent.NPCType<NPCs.Bosses.GrandDad>(),
+			    this,
+			    "Grand Dad",
+                (Func<bool>)(() => PaSWorld.downedGrandDad),
+			    ModContent.ItemType<Mario7>(),
+			    new List<int> { ModContent.ItemType<Items.Misc.GrandDadMusicBox>() },
+			    new List<int> { ModContent.ItemType<Items.BossBags.GrandDadBag>(), ModContent.ItemType<Items.Weapons.the7>(), ModContent.ItemType<Items.Weapons.DaedalusSevenbow>(), ModContent.ItemType<Items.Weapons.SevenShortsword>() },
+			    "Spawn by using [i:" + ModContent.ItemType<Mario7>() + "].",
+			    "Grand Dad has turned everyone into flint and stones...",
+			    "PissAndShit/NPCs/Bosses/GrandDad_Still",
+			    "PissAndShit/NPCs/Bosses/GrandDad_Head_Boss"
+                );
+
+                bossChecklist.Call(
+                "AddBoss",
+			    6.5f,
+			    ModContent.NPCType<NPCs.Bosses.Hive>(),
+			    this,
+			    "Hive",
+                (Func<bool>)(() => PaSWorld.downedHive),
+			    ModContent.ItemType<HiveSummon>(),
+			    0,
+			    new List<int> { ModContent.ItemType<Items.BossBags.HiveBag>(), ModContent.ItemType<Items.Weapons.BeeBook>(), ModContent.ItemType<Items.Weapons.BeeBasher>(), ModContent.ItemType<Items.Weapons.BeeTime>() },
+			    "Spawn by using [i:" + ModContent.ItemType<HiveSummon>() + "].",
+			    "The Hive's minions have stung everyone to death...",
+			    "PissAndShit/NPCs/Bosses/Hive",
+			    "PissAndShit/NPCs/Bosses/Hive_Head_Boss"
+                );
             }
-	    FargosMutantMod = ModLoader.GetMod("Fargowiltas");
+	        FargosMutantMod = ModLoader.GetMod("Fargowiltas");
             if (FargosMutantMod != null)
             {
 	        	FargosMutantMod.Call("AddSummon", 5.5f, "PissAndShit", "YoungWorm", (Func<bool>)(() => PaSWorld.downedYoungDuke), 150000);
