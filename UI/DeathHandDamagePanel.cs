694c1c1254e8939e5882843e66a591b38b1d0d38
Shardion <ShardionsSink@protonmail.com>
2020-09-25 03:28:52 -0700

    this might work?

--- UI/DeathHandDamagePanel.cs
+++ UI/DeathHandDamagePanel.cs
@@ -1,13 +1,20 @@
 using Terraria.UI;
 using Microsoft.Xna.Framework;
-using Terraria;
-using System;
-using Terraria.GameContent.UI.Elements;
+using Microsoft.Xna.Framework.Input;
 
 namespace PissAndShit.UI
 {
     class DeathHandDamagePanel : UIState
     {
+        public static MouseState curMouse;
+        public static MouseState oldMouse;
+        public static bool MouseClicked
+        {
+            get
+            {
+                return curMouse.LeftButton == ButtonState.Pressed && oldMouse.LeftButton == ButtonState.Released;
+            }
+        }
         public DraggablePanel BackgroundPanel;
         public UINumberPanel UINumPanel;
         public static bool DeathHandPanelVisible;
@@ -17,16 +24,16 @@ namespace PissAndShit.UI
 			BackgroundPanel.SetPadding(0);
 			BackgroundPanel.Left.Set(400f, 0f);
 			BackgroundPanel.Top.Set(50f, 0f);
-			BackgroundPanel.Width.Set(80f, 0f);
+			BackgroundPanel.Width.Set(100f, 0f);
 			BackgroundPanel.Height.Set(40f, 0f);
             BackgroundPanel.BackgroundColor = new Color(73, 94, 171);
 
             UINumPanel = new UINumberPanel();
             UINumPanel.SetPadding(0); // ?????
-            UINumPanel.Left.Set(20f, 0f);
-            UINumPanel.Top.Set(10f, 0f);
-            UINumPanel.Width.Set(60f, 0f);
-			UINumPanel.Height.Set(20f, 0f);
+            UINumPanel.Left.Set(40f, 0f);
+            UINumPanel.Top.Set(5f, 0f);
+            UINumPanel.Width.Set(50f, 0f);
+			UINumPanel.Height.Set(30f, 0f);
             BackgroundPanel.Append(UINumPanel);
 
             Append(BackgroundPanel);
@@ -38,5 +45,10 @@ namespace PissAndShit.UI
             NPC npc = Main.npc[PaSWorld.deathHandNPCIdentifier];
             npc.life =- 0;
         }*/
+        new public static void Update(GameTime gameTime)
+        {
+            oldMouse = curMouse;
+            curMouse = Mouse.GetState();
+        }
     }
 }
