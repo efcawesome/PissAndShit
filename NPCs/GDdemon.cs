710f2d6519bba60db83411ee52952095abd50a66
tigershark5639 <68576691+tigershark5639@users.noreply.github.com>
2021-01-11 21:06:50 -0500

    REAL!>!?!?

---
+++ NPCs/GDdemon.cs
@@ -0,0 +1,60 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace PissAndShit.NPCs
+{
+    public class GDdemon : ModNPC
+    {
+        private int frameNum = 0;
+        private int frameTimer = 0;
+        //int timer = 0;
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Froge");
+            NPCID.Sets.MustAlwaysDraw[npc.type] = true;
+            Main.npcFrameCount[npc.type] = 53;
+        }
+
+        public override void SetDefaults()
+        {
+            npc.width = 128;
+            npc.height = 117;
+            npc.damage = 30;
+            npc.defense = 10;
+            npc.lifeMax = 2000;
+            npc.HitSound = SoundID.NPCHit1;
+            npc.DeathSound = SoundID.NPCHit2;
+            npc.value = 60f;
+            npc.knockBackResist = 0.5f;
+            npc.aiStyle = 5;
+            npc.lavaImmune = true;
+            npc.noGravity = true;
+            npc.noTileCollide = true;
+        }
+
+        public override void AI()
+        {
+            frameTimer++;
+        }
+
+        public override void FindFrame(int frameHeight)
+        {
+            npc.spriteDirection = npc.direction;
+
+            if (frameTimer == 2)
+            {
+                frameNum++;
+                frameTimer = 0;
+            }
+
+            if (frameNum == 53)
+            {
+                frameNum = 0;
+            }
+
+            npc.frame.Y = frameNum * frameHeight;
+        }
+    }
+}
